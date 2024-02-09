36004e30c1f3f9e53dfff3e33dd9086d0f1b2132
rileywclarke <rclarke1995@gmail.com>
2024-02-09 13:46:04 -0500

    adding test suggested in issue #6

---
+++ tests/test_lightcurve.py
@@ -0,0 +1,14 @@
+"""Tests for Lightcurve objects created by lightcurve.py"""
+
+import numpy as np
+
+def test_add_observations():
+    from lcanalyzer import lightcurve
+    np.random.seed(999)
+    test_mjds = np.arange(50000,55000,1)
+    test_mags = np.random.random(len(test_mjds))
+    lc_obj = lightcurve.Lightcurve(obj_id = 123, mjds=None, mags=None)
+    lc_obj.add_observations(mjds = test_mjds, mags = test_mags)
+
+    assert lc_obj.lc['mjd'] is not None
+    assert lc_obj.lc['mag'] is not None
